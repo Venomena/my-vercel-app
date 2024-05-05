2ae2725ddedb0079e28d681d278399221ac2df42
Venomena <victor.gulchenko@gmail.com>
2024-05-05 18:53:01 +0200

    finally working?

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 require('dotenv').config();
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path'); // Add this to handle file paths
-const queryRouter = require('./api/query');
+const path = require('path');
+const queryRouter = require('./api/query');  // Ensure this is exporting an Express router
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use('/api/query', queryRouter);
+app.use('/api', queryRouter);  // Changed to use '/api' as base path for the router
 
 // Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the index.html file when accessing the root route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Catch-all for 404 Not Found responses
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 app.listen(port, () => {
